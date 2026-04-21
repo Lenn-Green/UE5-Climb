23a5c229824111dc17c874af19a390cb8471428c
汤玥 <tangyue@xmfunny.com>
2026-04-21 16:14:26 +0800

    feat: add climbing input binding framework

--- Source/Climb/Private/Character/ClimbingCharacter.cpp
+++ Source/Climb/Private/Character/ClimbingCharacter.cpp
@@ -1,7 +1,11 @@
 #include "Character/ClimbingCharacter.h"
 
 #include "Components/ClimbingMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "InputAction.h"
+#include "InputActionValue.h"
 
 AClimbingCharacter::AClimbingCharacter(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer.SetDefaultSubobjectClass<UClimbingMovementComponent>(ACharacter::CharacterMovementComponentName))
@@ -15,6 +19,14 @@ void AClimbingCharacter::BeginPlay()
 
 	const UCharacterMovementComponent* Movement = GetCharacterMovement();
 	SetClimbingState(Movement && Movement->IsMovingOnGround() ? EClimbingState::Grounded : EClimbingState::Falling);
+	AddClimbingInputMappingContext();
+}
+
+void AClimbingCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	AddClimbingInputMappingContext();
+	BindClimbingInputActions(PlayerInputComponent);
 }
 
 void AClimbingCharacter::EnterClimbing()
@@ -58,6 +70,126 @@ bool AClimbingCharacter::IsClimbing() const
 UClimbingMovementComponent* AClimbingCharacter::GetClimbingMovementComponent() const
 {
 	return Cast<UClimbingMovementComponent>(GetCharacterMovement());
+}
+
+FVector2D AClimbingCharacter::GetClimbCoMInput() const
+{
+	return ClimbCoMInput;
+}
+
+FVector2D AClimbingCharacter::GetClimbLimbProbeInput() const
+{
+	return ClimbLimbProbeInput;
+}
+
+float AClimbingCharacter::GetLeftGripInput() const
+{
+	return LeftGripInput;
+}
+
+float AClimbingCharacter::GetRightGripInput() const
+{
+	return RightGripInput;
+}
+
+void AClimbingCharacter::AddClimbingInputMappingContext() const
+{
+	if (!ClimbingInputMappingContext)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Climbing input mapping context is not assigned on %s."), *GetName());
+		return;
+	}
+
+	const APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (!PlayerController)
+	{
+		return;
+	}
+
+	const ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+	if (!LocalPlayer)
+	{
+		return;
+	}
+
+	if (UEnhancedInputLocalPlayerSubsystem* InputSubsystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+	{
+		InputSubsystem->AddMappingContext(ClimbingInputMappingContext, ClimbingInputMappingPriority);
+	}
+}
+
+void AClimbingCharacter::BindClimbingInputActions(UInputComponent* PlayerInputComponent)
+{
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (!EnhancedInputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AClimbingCharacter requires UEnhancedInputComponent for climbing controls."));
+		return;
+	}
+
+	if (ClimbCoMMoveAction)
+	{
+		EnhancedInputComponent->BindAction(ClimbCoMMoveAction, ETriggerEvent::Triggered, this, &AClimbingCharacter::HandleClimbCoMMove);
+		EnhancedInputComponent->BindAction(ClimbCoMMoveAction, ETriggerEvent::Completed, this, &AClimbingCharacter::HandleClimbCoMMoveCompleted);
+	}
+
+	if (ClimbLimbProbeAction)
+	{
+		EnhancedInputComponent->BindAction(ClimbLimbProbeAction, ETriggerEvent::Triggered, this, &AClimbingCharacter::HandleClimbLimbProbe);
+		EnhancedInputComponent->BindAction(ClimbLimbProbeAction, ETriggerEvent::Completed, this, &AClimbingCharacter::HandleClimbLimbProbeCompleted);
+	}
+
+	if (ClimbLeftGripAction)
+	{
+		EnhancedInputComponent->BindAction(ClimbLeftGripAction, ETriggerEvent::Triggered, this, &AClimbingCharacter::HandleClimbLeftGrip);
+		EnhancedInputComponent->BindAction(ClimbLeftGripAction, ETriggerEvent::Completed, this, &AClimbingCharacter::HandleClimbLeftGripCompleted);
+	}
+
+	if (ClimbRightGripAction)
+	{
+		EnhancedInputComponent->BindAction(ClimbRightGripAction, ETriggerEvent::Triggered, this, &AClimbingCharacter::HandleClimbRightGrip);
+		EnhancedInputComponent->BindAction(ClimbRightGripAction, ETriggerEvent::Completed, this, &AClimbingCharacter::HandleClimbRightGripCompleted);
+	}
+}
+
+void AClimbingCharacter::HandleClimbCoMMove(const FInputActionValue& Value)
+{
+	ClimbCoMInput = Value.Get<FVector2D>();
+}
+
+void AClimbingCharacter::HandleClimbCoMMoveCompleted(const FInputActionValue& Value)
+{
+	ClimbCoMInput = FVector2D::ZeroVector;
+}
+
+void AClimbingCharacter::HandleClimbLimbProbe(const FInputActionValue& Value)
+{
+	ClimbLimbProbeInput = Value.Get<FVector2D>();
+}
+
+void AClimbingCharacter::HandleClimbLimbProbeCompleted(const FInputActionValue& Value)
+{
+	ClimbLimbProbeInput = FVector2D::ZeroVector;
+}
+
+void AClimbingCharacter::HandleClimbLeftGrip(const FInputActionValue& Value)
+{
+	LeftGripInput = Value.Get<float>();
+}
+
+void AClimbingCharacter::HandleClimbLeftGripCompleted(const FInputActionValue& Value)
+{
+	LeftGripInput = 0.0f;
+}
+
+void AClimbingCharacter::HandleClimbRightGrip(const FInputActionValue& Value)
+{
+	RightGripInput = Value.Get<float>();
+}
+
+void AClimbingCharacter::HandleClimbRightGripCompleted(const FInputActionValue& Value)
+{
+	RightGripInput = 0.0f;
 }
 
 void AClimbingCharacter::SetClimbingState(EClimbingState NewState)
--- Source/Climb/Public/Character/ClimbingCharacter.h
+++ Source/Climb/Public/Character/ClimbingCharacter.h
@@ -5,7 +5,10 @@
 #include "Data/ClimbingTypes.h"
 #include "ClimbingCharacter.generated.h"
 
+struct FInputActionValue;
 class UClimbingMovementComponent;
+class UInputAction;
+class UInputMappingContext;
 
 UCLASS()
 class CLIMB_API AClimbingCharacter : public ACharacter
@@ -33,12 +36,65 @@ public:
 	UFUNCTION(BlueprintPure, Category="Climbing|Movement")
 	UClimbingMovementComponent* GetClimbingMovementComponent() const;
 
+	UFUNCTION(BlueprintPure, Category="Climbing|Input")
+	FVector2D GetClimbCoMInput() const;
+
+	UFUNCTION(BlueprintPure, Category="Climbing|Input")
+	FVector2D GetClimbLimbProbeInput() const;
+
+	UFUNCTION(BlueprintPure, Category="Climbing|Input")
+	float GetLeftGripInput() const;
+
+	UFUNCTION(BlueprintPure, Category="Climbing|Input")
+	float GetRightGripInput() const;
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Climbing|State")
 	EClimbingState ClimbingState = EClimbingState::Falling;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	TObjectPtr<UInputMappingContext> ClimbingInputMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	int32 ClimbingInputMappingPriority = 1;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	TObjectPtr<UInputAction> ClimbCoMMoveAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	TObjectPtr<UInputAction> ClimbLimbProbeAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	TObjectPtr<UInputAction> ClimbLeftGripAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Climbing|Input")
+	TObjectPtr<UInputAction> ClimbRightGripAction;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Climbing|Input")
+	FVector2D ClimbCoMInput = FVector2D::ZeroVector;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Climbing|Input")
+	FVector2D ClimbLimbProbeInput = FVector2D::ZeroVector;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Climbing|Input")
+	float LeftGripInput = 0.0f;
+
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Climbing|Input")
+	float RightGripInput = 0.0f;
+
 private:
+	void AddClimbingInputMappingContext() const;
+	void BindClimbingInputActions(UInputComponent* PlayerInputComponent);
+	void HandleClimbCoMMove(const FInputActionValue& Value);
+	void HandleClimbCoMMoveCompleted(const FInputActionValue& Value);
+	void HandleClimbLimbProbe(const FInputActionValue& Value);
+	void HandleClimbLimbProbeCompleted(const FInputActionValue& Value);
+	void HandleClimbLeftGrip(const FInputActionValue& Value);
+	void HandleClimbLeftGripCompleted(const FInputActionValue& Value);
+	void HandleClimbRightGrip(const FInputActionValue& Value);
+	void HandleClimbRightGripCompleted(const FInputActionValue& Value);
 	void SetClimbingState(EClimbingState NewState);
 };
